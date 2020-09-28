76704a565e8641d34c49c5660eb89983170a6cfa
Namit <43697251+namit98@users.noreply.github.com>
2020-09-28 15:37:47 +0530

    Updates

---
+++ precache-manifest.0b0954487e285abe6bc73732e3d035b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb3b794f0d2e73b740252e8617155d4",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "8cc02cd4500524ec36c5",
+    "url": "/React-ToDo-App/static/css/main.86a26a76.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/React-ToDo-App/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc02cd4500524ec36c5",
+    "url": "/React-ToDo-App/static/js/main.c970b119.chunk.js"
+  },
+  {
+    "revision": "1a118a873dfd5e6996ad",
+    "url": "/React-ToDo-App/static/js/runtime-main.2177da3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-ToDo-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
